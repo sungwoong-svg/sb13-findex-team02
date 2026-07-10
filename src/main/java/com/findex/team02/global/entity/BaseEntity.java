3e003ddbc5369b2409e070e30473cbaf19855971
sungwoong-svg <skarn506@gmail.com>
2026-07-10 14:12:58 +0900

    feat: 공통 엔티티 기본 구조 추가

---
+++ src/main/java/com/findex/team02/global/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.findex.team02.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private Instant createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private Instant updatedAt;
+
+}
