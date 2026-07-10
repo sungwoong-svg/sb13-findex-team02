a1ac7005f2d8722574fee79318ceba4603a70886
sungwoong-svg <skarn506@gmail.com>
2026-07-10 16:11:52 +0900

    feat: 지수 데이터 커서 조회용 리포지토리 인터페이스 추가

--- src/main/java/com/findex/team02/indexdata/repository/IndexDataRepository.java
+++ src/main/java/com/findex/team02/indexdata/repository/IndexDataRepository.java
@@ -3,6 +3,7 @@ package com.findex.team02.indexdata.repository;
 import com.findex.team02.indexdata.entity.IndexData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IndexDataRepository extends JpaRepository<IndexData, Long> {
+public interface IndexDataRepository extends JpaRepository<IndexData, Long>,
+    IndexDataRepositoryCustom {
 
 }
